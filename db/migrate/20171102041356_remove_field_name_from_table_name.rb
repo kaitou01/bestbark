ffd59eaabaafff1a94f47607d4403e7ea749aea1
Kenson Catibog <kensoncatibog23@gmail.com>
2017-11-02 04:16:11 +0000

    Removed unwanted column

---
+++ db/migrate/20171102041356_remove_field_name_from_table_name.rb
@@ -0,0 +1,5 @@
+class RemoveFieldNameFromTableName < ActiveRecord::Migration[5.1]
+  def change
+    remove_column :line_items, :product_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171102040510) do
+ActiveRecord::Schema.define(version: 20171102041356) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -63,7 +63,6 @@ ActiveRecord::Schema.define(version: 20171102040510) do
   create_table "line_items", force: :cascade do |t|
     t.integer "quantity"
     t.decimal "price"
-    t.integer "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
