020280f04409cdde79ba498d4d4ce40c11fb7b14
Kenson Catibog <kensoncatibog23@gmail.com>
2017-11-02 03:59:59 +0000

    Added customer model

---
+++ db/migrate/20171102035944_create_customers.rb
@@ -0,0 +1,14 @@
+class CreateCustomers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :customers do |t|
+      t.string :name
+      t.string :username
+      t.string :password
+      t.string :address
+      t.string :city
+      t.string :postal_code
+
+      t.timestamps
+    end
+  end
+end
