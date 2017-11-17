0c213fe82107428179785164ad54298b742efd65
Kenson Catibog <kensoncatibog23@gmail.com>
2017-11-17 02:23:00 +0000

    Added ability to add and remove items to cart using sessions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,8 @@
 # Controller for the products model
 class ProductsController < ApplicationController
+  before_action :initialize_session
+  before_action :load_shopping_cart, only: [:index, :show]
+
   def index
     # @products = Product.order(:name).page(params[:page]).per(5)
     @products = Product.page(params[:page]).per(5).search(params[:search], params[:category])
@@ -12,5 +15,33 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
+  end
+
+  def add_to_cart
+    id = params[:id].to_i
+    session[:products_to_buy] << id unless session[:products_to_buy].include?(id)
+    redirect_to product_path(id), notice: "Successfully added product to cart."
+  end
+
+  def remove_item_from_cart
+    id = params[:id].to_i
+    session[:products_to_buy].delete(id)
+
+    redirect_to product_path(id), notice: "Marked Customer As Called"
+  end
+
+  def empty_cart
+    session[:products_to_buy] = []
+    redirect_to root_url, notice: "Cart emptied."
+  end
+
+  private
+
+  def initialize_session
+    session[:products_to_buy] ||= []
+  end
+
+  def load_shopping_cart
+    @products_to_buy = Product.find(session[:products_to_buy])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,16 @@ Rails.application.routes.draw do
 
   get 'contact', to: 'pages#contact', as: 'contact'
 
+  resources :products, only: [:show] do
+    member do
+      post :add_to_cart
+      post :remove_item_from_cart
+    end
+    collection do
+      post :empty_cart
+    end
+  end
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
